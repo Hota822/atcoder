892adcc4b43565c1bb5105cf02e63228ba57a212
hota822 <hota822@gmail.com>
2020-01-27 01:52:08 +0000

    practice_a ver 2

--- beginner/practice_a/src/main.rs
+++ beginner/practice_a/src/main.rs
@@ -1,3 +1,17 @@
+use std::io::stdin;
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    let mut stack = Vec::new();
+    stdin().read_line(&mut input).expect("s");
+    stack.push(match input.trim().parse::<i32>() {Ok(num) => num,Err(_) => return,});
+    stdin().read_line(&mut input).expect("s");
+    for chr in input.split_whitespace() {
+        stack.push(match chr.trim().parse::<i32>() {Ok(num) => num,Err(_) => return,})
+    }
+    stdin().read_line(&mut input).expect("s");
+    let mut sum: i32 =  0;
+    for i in stack {
+        sum += i;
+    }
+    println!("{}",input);
 }
