4c81e3600c16d9a0934f9e3235eb719b6f8a4c3a
hota822 <hota822@gmail.com>
2020-01-27 05:01:32 +0000

    practice_a ver 3

--- beginner/practice_a/src/main.rs
+++ beginner/practice_a/src/main.rs
@@ -1,17 +1,16 @@
 use std::io::stdin;
 fn main() {
+    let mut stack = [0; 3];
     let mut input = String::new();
-    let mut stack = Vec::new();
-    stdin().read_line(&mut input).expect("s");
-    stack.push(match input.trim().parse::<i32>() {Ok(num) => num,Err(_) => return,});
-    stdin().read_line(&mut input).expect("s");
-    for chr in input.split_whitespace() {
-        stack.push(match chr.trim().parse::<i32>() {Ok(num) => num,Err(_) => return,})
+    {
+        let mut input = String::new();
+        for _i in 0..2 {
+            stdin().read_line(&mut input);
+        }
+        for (i, chr) in input.split_whitespace().enumerate() {
+            stack[i] = match chr.trim().parse::<i32>() {Ok(num) => num,Err(_) => return,}
+        }
     }
-    stdin().read_line(&mut input).expect("s");
-    let mut sum: i32 =  0;
-    for i in stack {
-        sum += i;
-    }
-    println!("{}",input);
+    stdin().read_line(&mut input);
+    println!("{} {}",stack[0] + stack[1] + stack[2], input);
 }
