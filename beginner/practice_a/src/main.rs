8df002681c73d1544616af876b78e5dc3e32e617
hota822 <hota822@gmail.com>
2020-01-28 04:17:26 +0000

    practice_a ver 4

--- beginner/practice_a/src/main.rs
+++ beginner/practice_a/src/main.rs
@@ -1,16 +1,15 @@
-use std::io::stdin;
 fn main() {
-    let mut stack = [0; 3];
+    let mut stack = 0;
     let mut input = String::new();
     {
         let mut input = String::new();
         for _i in 0..2 {
-            stdin().read_line(&mut input);
+            std::io::stdin().read_line(&mut input);
         }
-        for (i, chr) in input.split_whitespace().enumerate() {
-            stack[i] = match chr.trim().parse::<i32>() {Ok(num) => num,Err(_) => return,}
+        for chr in input.split_whitespace() {
+            stack += match chr.parse::<i32>() {Ok(num) => num,Err(_) => return,}
         }
     }
-    stdin().read_line(&mut input);
-    println!("{} {}",stack[0] + stack[1] + stack[2], input);
+    std::io::stdin().read_line(&mut input);
+    println!("{} {}",stack, input.trim());
 }
