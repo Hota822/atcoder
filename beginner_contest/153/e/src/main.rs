d4fce5f9d5494370487e3c073d811b2d0df2e770
hota822 <hota822@gmail.com>
2020-02-06 06:55:10 +0000

    Update: beginner conteset 153/e (WA)

--- beginner_contest/153/e/src/main.rs
+++ beginner_contest/153/e/src/main.rs
@@ -12,22 +12,66 @@ fn main() {
         stack[i] = j.parse::<i32>().unwrap();
     }
 
-    let mut vec: Vec<[i32; 3]> = Vec::new();
-
-    for k in 0..stack[1] {
-        {
-            let mut input = String::new();
-            stdin().read_line(&mut input);
-            values = input;
-        }
-        let pair = [0; 3];
-        for (i, j) in values.split_whitespace().enumerate() {
-            pair[i] = j.parse::<i32>().unwrap();
-        }
-        pair[2] = pair[0] / pair[1];
-        vec.push(pair);
+    let mut input = String::new();
+    for i in 0..stack[1] {
+        stdin().read_line(&mut input);
+    }
+    let mut input: Vec<i32> = input.split_whitespace()
+                                    .map(|x| x.parse::<i32>().unwrap())
+                                    .collect();
+    let mut damages: Vec<i32> = Vec::new();
+    let mut costs: Vec<i32> = Vec::new();
+    for i in 0..stack[1] {
+        costs.push(input.pop().unwrap());
+        damages.push(input.pop().unwrap());
     }
 
-
-    println!("{}", times);
+    let mut minimun_cost = 100000000;
+    let mut remained_life = 0;
+    let mut total_attack = 0;
+    let mut cost_for_i = 0;
+    for i  in 0..stack[1] {
+        total_attack = stack[0] % damages[i as usize];
+        cost_for_i = stack[0] / damages[i as usize] * costs[i as usize];
+        println!("damage of {} =================================",damages[i as usize] );
+        for j in 0..stack[1] {
+            println!("times: {}, danage: {}",j , damages[j as usize]);
+            remained_life = total_attack;
+            println!("before remain:{}",remained_life );
+            let mut cost = cost_for_i;
+            println!("before cost{}, stack: {}, damages:{}, costs:{}",cost, stack[0], damages[j as usize], costs[j as usize] );
+            while remained_life > 0 {
+                remained_life -= damages[j as usize];
+                cost += costs[j as usize];
+            }
+            println!("remained_life:{}", remained_life );
+            println!("costs{}",cost );
+            println!("====================");
+            if minimun_cost > cost { minimun_cost = cost}
+        }
+    }
+    println!("{}",minimun_cost );
 }
+
+// fn vector_sort(vec: &mut Vec<[i32; 2]>, index: &i32, value: &i32) {
+//     let minimum = 0;
+//     let mut new_vec: Vec<[i32; 2]> = Vec::new();
+//     for v in vec {
+//         if v[value] > minimum {}
+//     }
+// }
+
+// use std::io::stdin;
+// use std::collections::HashSet;
+
+// fn main() {
+//     let mut input = String::new();
+//     stdin().read_line(&mut input).unwrap();
+//     let times: i32 = match input.trim().parse() {Ok(num) => num, Err(_) => return,};
+//     let mut input = String::new();
+//     for i in 0..times {
+//         stdin().read_line(&mut input).unwrap();
+//     }
+//     let vec: HashSet<i32> = input.split_whitespace().map(|x| x.parse().unwrap()).collect();
+//     println!("{}", vec.len());
+// }
