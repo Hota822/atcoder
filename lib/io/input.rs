96686ff50631183cbdf22db68f7f92ebc6485f2c
hota822 <hota822@gmail.com>
2020-02-07 01:38:06 +0000

    Update: add comment

--- lib/io/input.rs
+++ lib/io/input.rs
@@ -1,6 +1,9 @@
 use std::io::stdin;
 
 fn main {}
+// range of using i32
+//     about 2 * 10^9 ～ -2 * 10^9
+//     ( -2,147,483,648 ～ 2,147,483,647 )
 
 // 使う前に check point の型を確認
 fn get_input_by_stack() -> [i32; 2] { // check point
