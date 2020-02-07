727d85861c64cc628ce3fb89bf2955900859ea8c
hota822 <hota822@gmail.com>
2020-02-07 10:02:31 +0000

    Add: ABC 153/f (CE)

--- beginner_contest/153/f/src/main.rs
+++ beginner_contest/153/f/src/main.rs
@@ -1,21 +1,17 @@
-use std::io::stdin;
+// use std::io::stdin;
 
 fn main() {
-    let vec = get_input_by_vec();
+    let vec: Vec<i32> = get_input_by_vec();
     // 0: N 体のモンスター
     // 1: D 爆弾の範囲
     // 2: A 爆弾のダメージ
-    let (mut each_hp_and_location, vector_size) = get_input_by_loop(vec[0]);
-    let mut monster_location_and_hp = vec![0; vector_size + 1];
-    let (mut location, mut hp) = (0, 0);
-    let mut locations = Vec::new();
-    for i in 0..vec[0] {
-        // println!("{}",i );
-        hp = each_hp_and_location.pop().unwrap();
-        location = each_hp_and_location.pop().unwrap();
-        // println!("location: {}, hp: {}",location, hp );
-        monster_location_and_hp[location as usize] = hp;
-        locations.push(location);
+    let mut each_hp_and_location = get_input_by_loop(vec[0]);
+    let mut locations: Vec<&i32> = Vec::new();
+
+    {
+        for v in &each_hp_and_location {
+            locations.push(&v[0]);
+        }
     }
 
     // print!("[");
@@ -26,11 +22,11 @@ fn main() {
 
     locations.sort();
     let mut used_bomb: i64 = 0;
-    let mut remained_monster = vec[0];
     let bomb_range = vec[1] * 2;
+    let mut effect_range = 0..0;
     let damage = vec[2];
-    let mut counter = 0;
-    let mut using_bomb_position = vector_size; // 暫定値
+    // let mut counter = 0;
+    let mut _using_bomb_position = 0; // 暫定値
 
     while  locations.len() > 0 {
         // counter += 1;
@@ -39,41 +35,51 @@ fn main() {
         // モンスターの位置を取得
         let next_monster_location = locations.pop().unwrap();
 
-        //if // HP==0(前のBombで死んだとき）の時はcontinue
-        if monster_location_and_hp[next_monster_location as usize] <= 0 {continue;}
+        // 対象モンスターのHPを取得
+        let mut hp: &i32 = &0;
+        {
+            for v in &each_hp_and_location {
+                if &v[0] != next_monster_location {continue;}
+                hp = &v[1];
+            }
+        }
+
+        // HP<=0(前のBombで死んだとき）の時はcontinue
+        if hp <= &0 {continue;}
 
         // BOMBダメージ計算（対象のモンスター）
         // 累積（巻き添え）ダメージを初期化
-        let mut accumulation_damage = 0;
+        let mut _accumulation_damage = 0;
 
         // BOM位置が範囲を外れた時はBOMの範囲で使用
-        if next_monster_location - (bomb_range / 2) < 0 {using_bomb_position = 0}
+        if next_monster_location - (bomb_range / 2) < 0 {_using_bomb_position = 0}
+
+        // 爆発の影響範囲計算
+        effect_range = (next_monster_location - bomb_range)..(next_monster_location + 1);
 
         //累積カウントの計算（Temp）
-        accumulation_damage = (monster_location_and_hp[next_monster_location as usize] + damage - 1) / damage;
+        _accumulation_damage = (hp + damage - 1) / damage;
 
         // 使用回数を計算
-        used_bomb += accumulation_damage as i64;
+        used_bomb += _accumulation_damage as i64;
 
-        accumulation_damage *= damage;
+        // 累積ダメージの計算
+        _accumulation_damage *= damage;
 
         // println!("  accumulation:{}",accumulation_damage );
         // println!("  next_monster_location: {}", next_monster_location);
         // BOMBダメージ計算（対象のモンスター）
-        for i in 0..(bomb_range + 1) {
-
-            // 範囲外に出たら終了
-            if next_monster_location < i {break;}
-
-            // bom positionの変更
-            let position = (next_monster_location -i) as usize;
-            // println!("target: {}",position );
-
-            // 巻き添えダメージの計算
-            monster_location_and_hp[position] -= accumulation_damage;
-
-            // 累積ダメージで、またはもともといない時は次のループに
-            if monster_location_and_hp[position] <= 0 {continue;}
+        {
+            for mut v in &mut each_hp_and_location {
+                // 範囲にいた時の処理
+                match v[0] {
+                    effect_range => {
+                        // 巻き添えダメージの計算
+                        v[1] -= _accumulation_damage;
+                        // 累積ダメージで、またはもともといない時は次のループに
+                    }
+                }
+            }
         }
     }
     println!("{}",used_bomb);
@@ -82,26 +88,33 @@ fn main() {
 
 fn get_input_by_vec() -> Vec<i32> {
     let mut input  = String::new();
-    std::io::stdin().read_line(&mut input);
-    let mut vec: Vec<i32> = input.split_whitespace()
+    std::io::stdin().read_line(&mut input).unwrap();
+    let mut _vec: Vec<i32> = input.split_whitespace()
                             .map(|x| x.parse().unwrap())
                             .collect();
-    vec
+    _vec
 }
 
-fn get_input_by_loop(loop_count: i32) -> (Vec<i32>, usize) {
+fn get_input_by_loop(loop_count: i32) -> Vec<Vec<i32>> {
     let mut input = String::new();
     let mut max = 0;
-    let mut temp = 0;
-    for i in 0..loop_count {
-        std::io::stdin().read_line(&mut input);
+    let mut vec: Vec<Vec<i32>> = Vec::new();
+    println!("start get" );
+    for _i in 0..loop_count {
+        std::io::stdin().read_line(&mut input).unwrap();
     }
-    (input.split_whitespace()
-         .map(|x| {
-             temp = x.parse::<i32>().unwrap();
-             if temp > max {max = temp;}
-             x.parse::<i32>().unwrap()
-         })
-         .collect()
-    , max as usize)
+    let mut temp: Vec<i32> = input.split_whitespace()
+         .map(|x| x.parse::<i32>().unwrap())
+         .collect();
+
+    let (mut x, mut y) = (0, 0);
+    loop{
+        x = match temp.pop() {
+            Some(v)  => v,
+            None => break
+        };
+        y = temp.pop().unwrap();
+        vec.push(vec![y, x]);
+    }
+    vec
 }
--- lib/io/input.rs
+++ lib/io/input.rs
@@ -1,17 +1,45 @@
 use std::io::stdin;
-
+// 中身の型を変えたいときはcheck pointを変更
 fn main {}
 // range of using i32
 //     about 2 * 10^9 ～ -2 * 10^9
 //     ( -2,147,483,648 ～ 2,147,483,647 )
 
-// 使う前に check point の型を確認
+
+//=====================================================================================
+// Single Line Input
+//=====================================================================================
 fn get_input_by_stack() -> [i32; 2] { // check point
     let mut input  = String::new();
     std::io::stdin().read_line(&mut input);
-    let mut stack: [i32; 2] = [0; 2]; // check point 
+    let mut stack: [i32; 2] = [0; 2]; // check point
     for (i, j ) in input.split_whitespace().enumerate() {
         stack[i] = j.parse().unwrap();
     }
     stack
 }
+
+fn get_input_by_vec() -> Vec<i32> { //checkpoint
+    let mut input  = String::new();
+    std::io::stdin().read_line(&mut input);
+    let mut vec: Vec<i32> = input.split_whitespace() // check point
+                            .map(|x| x.parse().unwrap())
+                            .collect();
+    vec
+}
+//=======================================================================================
+// multiple input
+//=======================================================================================
+fn get_input_by_loop(loop_count: i32) -> Vec<i32> {
+    let mut input = String::new();
+    for i in 0..loop_count {
+        std::io::stdin().read_line(&mut input);
+    }
+    input.split_whitespace()
+         .map(|x| x.parse::<i32>().unwrap())
+         .collect()
+}
+
+
+
+
