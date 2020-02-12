22f9ffd7dadda286d249a3b6edf8cb12e56ff884
hota822 <hota822@gmail.com>
2020-02-12 14:20:44 +0900

    Update: ABC 153/a review

--- beginner_contest/153/a/src/main.rs
+++ beginner_contest/153/a/src/main.rs
@@ -1,14 +1,11 @@
-use std::io::stdin;
-
 fn main() {
+    use std::collections::VecDeque;
     let mut input = String::new();
-    stdin().read_line(&mut input);
-    let mut stack = [0; 2];
-    for (i, j) in input.split_whitespace().enumerate() {
-        stack[i] = j.parse().unwrap()
-    }
-    let mut times = stack[0] / stack[1];
-    if stack[0] % stack[1] != 0 && stack[1] != 1 {times+=1}
-    if stack[0] < stack[1] { times = 1};
-    println!("{}", times);
+    std::io::stdin().read_line(&mut input);
+    let mut vec: Vec<i32> = Vec::with_capacity(2);
+    vec = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<i32>>();
+    println!("{}", (vec[0] + vec[1] -1) / vec[1] );
 }
