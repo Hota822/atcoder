fba408c8170115fac182ce3a9f69e725e12ceb77
hota822 <hota822@gmail.com>
2020-02-14 18:42:11 +0900

    Add: create directory for ABC 154

---
+++ beginner_contest/154/a/src/main.rs
@@ -0,0 +1,27 @@
+use std::io::stdin;
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input);
+    let mut vec = Vec::with_capacity(2);
+    vec = input.clone().split_whitespace()
+        .collect();
+
+    input.clear();
+    stdin().read_line(&mut input);
+    let mut num = Vec::with_capacity(2);
+    num = input.split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect::<Vec<i32>>();
+
+    input.clear();
+    stdin().read_line(&mut input);
+    let mut target = Vec::with_capacity(1);
+    target = input.clone().split_whitespace()
+        .collect();
+    
+    if target[0] == vec[0] {
+        println!("{} {}",num[0] -1, num[1] );
+    }
+
+}
